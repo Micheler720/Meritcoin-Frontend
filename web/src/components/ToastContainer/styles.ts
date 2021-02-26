f3d8e50fbcfdcef929978a9c32015af2c04c897a
Michele Ribeiro <48379411+Micheler720@users.noreply.github.com>
2021-02-26 12:00:33 -0300

    Delete web directory

--- web/src/components/ToastContainer/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  top: 0.5rem;
-  right: 0.5rem;
-  position: absolute;
-`;
--- web/src/pages/Register/GroupUser/ModalUpdateUser/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Content = styled.div`
-  padding: 1rem;
-  position: relative;
-  margin-top: 2rem;
-`;
